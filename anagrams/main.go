3e221cd52d31a29ca6c5923009ec0c54513cf026
Phil Hanna <ph1204@gmail.com>
2023-02-09 22:01:25 -0500

    Added go:embed to include sigmap.json in executable

---
+++ anagrams/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/philhanna/anagrams"
+)
+//go:embed sigmap.json
+var f embed.FS
+
+// Takes a string from the command line and finds all words whose
+// letters are contained in the string. Only words of length
+// greater than or equal to 3 are included. Output is written
+// to stdout.
+//
+
+func main() {
+
+	// Get the word from the command line
+	if len(os.Args) == 1 {
+		fmt.Fprintln(os.Stderr, "No word specified")
+		os.Exit(1)
+	}
+	word := os.Args[1]
+
+	// Start loading the dictionary
+	loaded := make(chan error)
+	sigmap := make(map[string][]string)
+	go loadDictionary(loaded, &sigmap)
+
+	// Wait for dictionary load to complete
+	err := <-loaded
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not load signature map: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Examine all subsets of the word with length >= 3 and store them in a map
+	outset := make(map[string]int)
+	subsets := anagrams.AllSubsets(word)
+	for _, subset := range subsets {
+
+		// Compute the subset's signature
+		signature := anagrams.SignatureOf(subset)
+
+		// Look up all words with the same signature
+		list, ok := sigmap[signature]
+		if ok {
+			for _, newWord := range list {
+				outset[newWord] += 1
+			}
+		}
+	}
+
+	// Sort the output set and write it to stdout
+	keys := make([]string, 0, len(outset))
+	for key := range outset {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		fmt.Printf("%s\n", key)
+	}
+}
+
+// Loads the signature map
+func loadDictionary(loaded chan error, pSigmap *map[string][]string) {
+	jsonBytes, err := f.ReadFile("sigmap.json")
+	if err != nil {
+		loaded <- err
+		return
+	}
+	err = json.Unmarshal(jsonBytes, pSigmap)
+	loaded <- err
+}
